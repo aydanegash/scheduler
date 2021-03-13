8436e28541749d3c4e279ca4854261119e86cf31
Ayda Negash <aydanegash@gmail.com>
2021-03-12 21:52:57 -0600

    display time function and save and display function

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+ //Variable
+ var timeDisplayEl = $("#display-time");
+ var currentHour = getHour();
+ console.log("testing")
+function displayTime() {
+    var now = moment().format("MMMM Do YYYY, hh:MM:SS a");
+    timeDisplayEl.text(now);
+    console.log(now);
+}
+
+displayTime();
+setInterval(displayTime, 1000);
+
+function getHour() {
+    return moment().hour();
+}
+//Current Date and Time
+
+//Display Current Date and Time
+
+//When type text and press save button, saves to local storage (event listener?)
+
+    $('.saveBtn').on("click", function(){
+        alert("i clicked on the save buton")
+        var textArea  = $(this).parent().find('.past').val();
+        var id = $(this).parent().find('.past').attr("id")
+        console.log(textArea, id)
+        localStorage.setItem(id,textArea)
+        // grab the text tthat the user typed and save it to localStorage
+
+    });
+//get text from text area and display in textbox
+    $("#9-textarea").val(localStorage.getItem("9AM"));
+    $("#10-textarea").val(localStorage.getItem("10AM"));
+    $("#11-textarea").val(localStorage.getItem("11AM"));
+    $("#12-textarea").val(localStorage.getItem("12AM"));
+    $("#1-textarea").val(localStorage.getItem("1PM"));
+    $("#2-textarea").val(localStorage.getItem("2PM"));
+    $("#3-textarea").val(localStorage.getItem("3PM"));
+    $("#4-textarea").val(localStorage.getItem("4PM"));
+    $("#5-textarea").val(localStorage.getItem("5PM"));
+    
+
+
+
+//Color changes to grey when  timeblock past current time
+
+//Color changes to red when timeblock at current time
+
+//Color changes to green when timeblock is after current time
